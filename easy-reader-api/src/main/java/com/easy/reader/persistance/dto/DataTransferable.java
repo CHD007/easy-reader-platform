158ccad33e208584757d8435ae80489193daaf78
chd007 <chernyshov777@gmail.com>
2017-04-15 22:22:37 +0300

    fix for dto assembler

--- easy-reader-api/src/main/java/com/easy/reader/persistance/dto/DataTransferable.java
+++ easy-reader-api/src/main/java/com/easy/reader/persistance/dto/DataTransferable.java
@@ -1,12 +1,14 @@
 package com.easy.reader.persistance.dto;
 
+import com.easy.reader.persistance.entity.BaseEntity;
+
 import java.io.Serializable;
 
 /**
  * Assembler for DTO.
  * @author dchernyshov
  */
-public interface DataTransferable<Entity, Wrapper extends Serializable> {
+public interface DataTransferable<Entity extends BaseEntity, Wrapper extends Serializable> {
     /**
      * @param entity entity for which DTO is created
      * @return DTO для Entity
