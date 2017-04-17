fbc964d506fd05a9e77e1d7ec1b3b7820fb00f87
Lubov <lubovruzanova@gmail.com>
2017-04-17 13:50:35 +0300

    part with poi - ok

--- easy-reader-api/src/main/java/com/easy/reader/exportFiles/exportExcelFiles.java
+++ easy-reader-api/src/main/java/com/easy/reader/exportFiles/exportExcelFiles.java
@@ -1,11 +1,17 @@
 package com.easy.reader.exportFiles;
 
+import com.easy.reader.persistance.entity.Book;
+import com.easy.reader.persistance.entity.BookWord;
+import com.easy.reader.persistance.entity.UserWord;
+import com.easy.reader.persistance.entity.Word;
 import jxl.Workbook;
+import jxl.format.CellFormat;
 import jxl.write.Label;
 import jxl.write.WritableSheet;
 import jxl.write.WritableWorkbook;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by kisa on 12.04.2017.
@@ -30,7 +36,18 @@ public class exportExcelFiles {
     {
         try {
             WritableWorkbook workbook = Workbook.createWorkbook(new File(fileName));
-            workbook.createSheet("firstSheet", 0);
+            WritableSheet copySheet = workbook.createSheet("My words sheet", 0);
+
+            Label word = new Label(0, 0 , "word");
+            copySheet.addCell(word);
+            Label transcription = new Label(1, 0 , "transcription");
+            copySheet.addCell(transcription);
+            Label translation = new Label(2, 0 , "translation");
+            copySheet.addCell(translation);
+            Label book = new Label(3, 0 , "book");
+            copySheet.addCell(book);
+            Label context = new Label(4, 0 , "context");
+            copySheet.addCell(context);
             workbook.write();
             workbook.close();
         }
@@ -44,15 +61,35 @@ public class exportExcelFiles {
         try {
             Workbook wb = Workbook.getWorkbook(new File(fileName));
             WritableWorkbook copy = Workbook.createWorkbook(new File(fileName), wb);
-            WritableSheet copySheet = copy.getSheet(0);
-            Label label1 = new Label(1, 2 , "world!");
-            copySheet.addCell(label1);
+           //for testing system
+            DanyaClass myObj = new DanyaClass();
+            myObj.word = "table";
+            myObj.book = "Kitchen";
+            myObj.context = "Lets sing a song about table.";
+            myObj.transcript = "'teɪb(ə)l";
+            myObj.translate = "стол";
 
+            WritableSheet copySheet = copy.getSheet(0);
+            int rows = copySheet.getRows();
+
+
+            Label lWord = new Label(0, rows , myObj.word);
+
+            copySheet.addCell(lWord);
+            Label lTranscr = new Label(1, rows , myObj.transcript);
+            copySheet.addCell(lTranscr);
+            Label lTranslate = new Label(2, rows , myObj.translate);
+            copySheet.addCell(lTranslate);
+            Label lBook = new Label(3, rows , myObj.book);
+            copySheet.addCell(lBook);
+            Label lContext = new Label(4, rows , myObj.context);
+            copySheet.addCell(lContext);
             copy.write();
             copy.close();
         }
         catch (Exception e)
         {
+
             e.printStackTrace();
 
         }
