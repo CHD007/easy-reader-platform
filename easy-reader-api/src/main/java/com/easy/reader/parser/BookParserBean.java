75bb9eaa34ecc840d44ac04a6657ead92e1ce4c3
chd007 <chernyshov777@gmail.com>
2017-04-17 17:45:20 +0300

    pppparser

--- easy-reader-api/src/main/java/com/easy/reader/parser/BookParserBean.java
+++ easy-reader-api/src/main/java/com/easy/reader/parser/BookParserBean.java
@@ -1,7 +1,54 @@
 package com.easy.reader.parser;
 
+import javax.ejb.Stateless;
+import javax.xml.namespace.QName;
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.Attribute;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
- * Created by dchernyshov on 17.04.17.
+ * @author dchernyshov
  */
+@Stateless
 public class BookParserBean {
+
+    private static final QName P_TAG = new QName("p");
+    private Attribute paragraph;
+
+    private Set<String> words = new HashSet<String>();
+    private XMLInputFactory factory = XMLInputFactory.newInstance();
+
+    public Set<String> parse(File file) throws IOException, XMLStreamException {
+        FileInputStream in = new FileInputStream(file);
+        XMLEventReader eventReader = factory.createXMLEventReader(in);
+
+        while (eventReader.hasNext()) {
+            XMLEvent xmlEvent = eventReader.nextEvent();
+            switch (xmlEvent.getEventType()) {
+                case XMLStreamConstants.START_ELEMENT:
+                    processStartElement(xmlEvent.asStartElement());
+                    break;
+            }
+        }
+        return words;
+    }
+
+    private void processStartElement(StartElement element) {
+        if(element.getName().equals(P_TAG)) {
+            paragraph = element.getAttributeByName(P_TAG);
+            String paragraphValue = paragraph.getValue();
+            String[] split = paragraphValue.split(" ");
+            words.addAll(Arrays.asList(split));
+        }
+    }
 }
--- easy-reader-api/src/test/java/com/easy/reader/parser/BookParserBeanTest.java
+++ easy-reader-api/src/test/java/com/easy/reader/parser/BookParserBeanTest.java
@@ -1,7 +1,30 @@
 package com.easy.reader.parser;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.xml.stream.XMLStreamException;
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Set;
+
 /**
  * Created by dchernyshov on 17.04.17.
  */
-public class BookParserBean {
+public class BookParserBeanTest {
+
+    @Test
+    public void testParse() {
+        String file = "xmlParserTest.xml";
+        BookParserBean bookParserBean= new BookParserBean();
+        try {
+            Set<String> words = bookParserBean.parse(new File(file));
+            Iterator<String> iterator = words.iterator();
+        } catch (IOException ioException) {
+            Assert.fail();
+        } catch (XMLStreamException streamExceiptiom) {
+            Assert.fail();
+        }
+    }
 }
