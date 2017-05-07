368c8af9945c4acb5fd440de54f2f33d07fb4868
lDarki <titkov.evgen@gmail.com>
2017-05-07 13:49:32 +0300

    [ts-linter] - code sonar fix

--- easy-reader-gui/src/app/app.module.ts
+++ easy-reader-gui/src/app/app.module.ts
@@ -8,10 +8,10 @@ import {HomeComponent} from './home/home.component';
 import {routing} from './app.routing';
 
 import {removeNgStyles, createNewHosts} from '@angularclass/hmr';
-import {HeaderComponent} from "./home/header/header.component";
-import {LeftMenu} from "./home/left-menu/leftmenu.component";
-import {DropdownComponent} from "./home/header/dropdown/dropdown.component";
-import {OverviewComponent} from "./home/overview/overview.component";
+import {HeaderComponent} from './home/header/header.component';
+import {LeftMenu} from './home/left-menu/leftmenu.component';
+import {DropdownComponent} from './home/header/dropdown/dropdown.component';
+import {OverviewComponent} from './home/overview/overview.component';
 
 @NgModule({
   imports: [
