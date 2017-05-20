a8d8c85be19a2b039b1a4731c4b13bd0ee4ee6b8
lDarki <titkov.evgen@gmail.com>
2017-05-20 16:49:47 +0300

    [book-list] - upload form dropdown

--- easy-reader-api/src/main/java/com/easy/reader/rest/BookUploadService.java
+++ easy-reader-api/src/main/java/com/easy/reader/rest/BookUploadService.java
@@ -3,6 +3,8 @@ package com.easy.reader.rest;
 import com.easy.reader.parser.BookParseException;
 import com.easy.reader.parser.BookParserBean;
 import org.apache.log4j.Logger;
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
 
 import javax.ejb.EJB;
 import javax.ws.rs.*;
@@ -22,18 +24,26 @@ public class BookUploadService {
     private BookParserBean bookParserBean;
 
     @POST
-    @Consumes(MediaType.APPLICATION_OCTET_STREAM)
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
     @Produces(MediaType.TEXT_PLAIN)
-    public Response uploadFile(@QueryParam("fileName") String fileName, InputStream content) {
-        String[] split = fileName.split("\\.");
-        String fileType = split[split.length - 1];
-    
-        try {
-            bookParserBean.parseBook(content, fileName, fileType);
+    public Response uploadFile(@FormDataParam("uploadFile") InputStream uploadedInputStream,
+                               @FormDataParam("uploadFile") FormDataContentDisposition fileDetail) {
+        // check if all form parameters are provided
+        if (uploadedInputStream == null || fileDetail == null)
+            return Response.status(400).entity("Invalid form data").build();
+        else {
             return Response.ok().build();
-        } catch (BookParseException | IOException e) {
-            LOGGER.error("Error while parsing book", e);
-            return Response.status(Response.Status.UNSUPPORTED_MEDIA_TYPE).entity("Invalid file type").build();
         }
+
+//        String[] split = fileName.split("\\.");
+//        String fileType = split[split.length - 1];
+//
+//        try {
+//            bookParserBean.parseBook(content, fileName, fileType);
+//            return Response.ok().build();
+//        } catch (BookParseException | IOException e) {
+//            LOGGER.error("Error while parsing book", e);
+//            return Response.status(Response.Status.UNSUPPORTED_MEDIA_TYPE).entity("Invalid file type").build();
+//        }
     }
 }
--- easy-reader-gui/src/app/components/book-upload/booklibrary.component.ts
+++ easy-reader-gui/src/app/components/book-upload/booklibrary.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 import {Book} from "../../shared/book";
+import {Http, RequestOptions} from "@angular/http";
+import {Observable} from "rxjs/Observable";
 
 @Component({
   selector: 'book-library',
@@ -8,12 +10,41 @@ import {Book} from "../../shared/book";
 })
 export class BookLibraryComponent {
 
-  private books:Array<Book> = [];
+  private isUploadFormHidden: boolean = true;
+  private isFileSelected: boolean = false;
 
-  constructor(){
-    this.books.push(new Book("Alice in Wonderland",2453,876,1200));
-    this.books.push(new Book("The Story of Eugene Titkov",2453,876,1200));
-    this.books.push(new Book("Nothing but truth",2453,876,1200));
+  private books: Array<Book> = [];
+
+  constructor(private http: Http) {
+    this.books.push(new Book("Alice in Wonderland", 2453, 876, 1200));
+    this.books.push(new Book("The Story of Eugene Titkov", 2453, 876, 1200));
+    this.books.push(new Book("Nothing but truth", 2453, 876, 1200));
+  }
+
+  changeUploadFormState() {
+    this.isUploadFormHidden = !this.isUploadFormHidden;
+  }
+
+  changeUpload(event: any) {
+    this.isFileSelected = !this.isFileSelected;
+    let fileList: FileList = event.target.files;
+    if (fileList.length > 0) {
+      let file: File = fileList[0];
+      let formData: FormData = new FormData();
+      formData.append('uploadFile', file, file.name);
+      let headers = new Headers();
+      headers.append('Content-Type', 'multipart/form-data');
+      headers.append('Accept', 'application/json');
+      let options = new RequestOptions(<any>{headers: headers});
+
+      this.http.post("http://localhost:8079", formData, options)
+        .map(res => res.json())
+        .catch(error => Observable.throw(error))
+        .subscribe(
+          data => console.log(data),
+          error => console.log(error)
+        )
+    }
   }
 
 }
